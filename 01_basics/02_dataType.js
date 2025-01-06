f2e7dddfb6e4fb9ab1bfe55dcff991f38737f8e2
Mayankkumar1634 <mayankmks86@gmail.com>
2025-01-06 20:22:26 +0000

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,21 @@
+"use strict";// it treat all JS code as newer version
+
+console.log(5 + 3)
+console.log("Mayank")
+
+let name = "Mayank"
+let age = 23
+let isLoggedIn = true
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boleam => true/false
+//null => standalone value
+//undefined =
+//symbool => unique
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
