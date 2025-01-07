a9120560bc3f323eb0c03a9a602a0958f1c20e6f
Mayankkumar1634 <mayankmks86@gmail.com>
2025-01-07 15:10:52 +0000

    DataType Summary

---
+++ 01_basics/06_datatype_summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbool, BigInt
+
+const score = 100
+const scoreValue = 100.5
+
+const isLoggedin = false
+const outsideTemp = null
+
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber = 1334767548786455666
+
+// Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+const heroes = ["Mayank", "Anuj", "Ayush"];
+{
+    name: "Mayank"
+    age: 23
+}
+
+const myFunction = function(){
+    console.log("Hello World!");
+   
+}
+
+console.log(typeof outsideTemp);
