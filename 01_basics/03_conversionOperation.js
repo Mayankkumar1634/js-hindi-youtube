2dd72c8e3b9a43c82c5b9917f12bbb2230923a38
Mayankkumar1634 <mayankmks86@gmail.com>
2025-01-07 07:58:03 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Mayank"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "36" => 36
+// "36abc" => NaN
+// true => 1; false => 0
+
+let loggedIn = ""
+
+let booleanloggedIn = Boolean(loggedIn)
+console.log(booleanloggedIn) 
+
+// 1 => true; 0 => false
+// "" => false
+// "Mayank" => true
+
+let num = 45
+
+let strNum = String(num)
+console.log(strNum)
+console.log(typeof strNum)
+
